8b54647528dc88e02d201bcf31a066d7959add6c
thusithanjana <thusithanjana@gmail.com>
2023-09-09 11:07:21 +0530

    Init MainActivity.kt

--- app/src/main/java/com/example/maddtutorial07/DashboardActivity.kt
+++ app/src/main/java/com/example/maddtutorial07/DashboardActivity.kt
@@ -2,8 +2,11 @@ package com.example.maddtutorial07
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 
 class DashboardActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
--- app/src/main/java/com/example/maddtutorial07/MainActivity.kt
+++ app/src/main/java/com/example/maddtutorial07/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.maddtutorial07
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var edtUsername:EditText
+    lateinit var edtPassword:EditText
+    lateinit var btnLogin:Button
+    lateinit var btnRegister:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        edtUsername = findViewById(R.id.edtUsername)
+        edtPassword = findViewById(R.id.edtPassword)
+        btnLogin = findViewById(R.id.btnLogin)
+        btnRegister = findViewById(R.id.btnRegister)
+
+        btnLogin.setOnClickListener {
+            handleLogin()
+        }
+
+        btnRegister.setOnClickListener {
+            handleRegister()
+        }
+    }
+
+    private fun handleLogin(){
+
+    }
+
+    private fun handleRegister(){
+
     }
 }
